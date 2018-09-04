18918866502e5e6cb8c3d79e08c5c3b531875f5a
Tinedanzer <johnhillallen@gmail.com>
2018-09-03 23:06:45 -0500

    twitter works, as well as most of the code to get the others done!

--- README.md
+++ README.md
@@ -1,5 +1,62 @@
 # LIRIBot
 
-This application was designed to be a node command line app that takes in user parameters and gives you back data!
+LIRI is a Language Interpretation and Recognition Interface. LIRI will be a command line node app that takes in parameters and gives you back data.
 
 This liribot was designed to give you back your last tweets from twitter, information about movies from omdb, and information about songs from spotify.
+
+LIRI Node App
+A node command line app to demonstrate multiple different API calls.
+
+Installation Instructions
+
+1) Get neccessary files / dependencies
+
+ Git clone this repository.
+ Navigate to liri-node-app/bin, and type 'npm install' in the terminal, to install the dependencies in package.json
+
+2) Add your API keys
+
+ Create a file called .env in the parent directory of this repository.
+In that file, add the following text, replacing the values with your own keys
+
+# Spotify API keys
+
+SPOTIFY_ID=your-spotify-id
+SPOTIFY_SECRET=your-spotify-secret
+
+# Twitter API keys
+
+TWITTER_CONSUMER_KEY=your-twitter-consumer-key
+TWITTER_CONSUMER_SECRET=your-twitter-consumer-secret
+TWITTER_ACCESS_TOKEN_KEY=your-access-token-key
+TWITTER_ACCESS_TOKEN_SECRET=your-twitter-access-token-secret
+
+# OMDB key
+
+OMDB_KEY=your-key
+Usage---- COMMANDS
+
+* `node liri.js my-tweets`  <= programmed to have 10 tweets returned
+
+* `node liri.js spotify-this-song "song"`
+
+* `node liri.js movie-this "movie"`
+
+* `node liri.js do-what-it-says` <= This is controlled by the text inside random.txt
+
+Notable Concepts / Technologies Used
+API authorization using URI tokens, localStorage handler
+
+Async / Await functions
+
+Node dev environment
+
+fs
+
+twitter API
+
+spotify API
+
+fetch
+
+
--- liri.js
+++ liri.js
@@ -1,4 +1,58 @@
+//  .env file for keys on my private local storage. the require "dotenv" allows these files to be read
 require("dotenv").config();
+// for file storage
 
-var spotify = new Spotify(keys.spotify);
-var client = new Twitter(keys.twitter);
+// let fs = require('fs');
+
+// load the keys
+let keys= require("./keys.js");
+let Twitter= require("twitter");
+// let Spotify=require("node-spotify-api");
+// let spotify = new Spotify(keys.spotify);
+let client = new Twitter(keys.twitter);
+// let inquirer= require("inquirer");
+let request= require("request");
+// const omdbKey=(keys.omdb);
+
+// imports keys for each of these apis
+let user1 = process.argv[2];
+let user2 = process.argv[3];
+// sets the parameters for 10 tweets, and my username on Twitter
+var params = {screen_name: 'Tinedanzer2', count: 10};
+// function Tweets() {}
+Tweets=  ()=>{
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  if (!error) 
+    {
+// allows me to treat my tweets as an object, so I can print only the text data
+      for (let tweet of tweets){       
+    //   console.log('§§§§§§§§§§§§§§§§§§§§§§§§§');
+    // console.log(JSON.stringify(tweet.text, null, 0));
+    console.log(tweet.text);
+                            };
+    };
+});
+};
+Commands = (user1, user2) => {
+    switch (user1) {
+        case "my-tweets":
+            Tweets();
+            break;
+
+        case "spotify-this-song":
+            if (user2 === undefined) {
+                user2 = "The River";
+            };
+            getSong(user2);
+            break;
+
+        case "movie-this":
+            Movie();
+            break;
+
+        case "do-what-it-says":
+            doThings();
+            break;};
+    };
+Commands(user1, user2)
+// getTweets()
